ff6a61dce4c2b9695ea57b4dbb1b29179c8c1d78
Ren Wako <maggots00002@gmail.com>
2020-08-01 21:22:30 +0900

    最初のコミット

---
+++ store/index.js
@@ -0,0 +1,27 @@
+export const state = () => ({});
+
+export const actions = {
+  // nuxtServerInit is called by Nuxt.js before server-rendering every page
+  nuxtServerInit(
+    { dispatch, commit, getters, state, rootGetters, rootState },
+    {
+      isServer,
+      isClient,
+      isStatic,
+      isDev,
+      isHMR,
+      app,
+      store,
+      error,
+      env,
+      redirect,
+      req,
+      res
+    }
+  )
+  {
+  if (req.session && req.session.authUser) {
+    commit('SET_USER', req.session.authUser);
+  }
+  }
+};
